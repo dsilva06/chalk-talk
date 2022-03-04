4d365a3ac2860f7952b9565b54e3ce4a29adc617
Diego Silva <Diegosilva06092001@gmail.com>
2022-03-04 15:55:31 -0400

    backend done

--- server/middleware/jwt.middleware.js
+++ server/middleware/jwt.middleware.js
@@ -1,22 +1,26 @@
-const jwt = require ("express-jwt") ;
-// Instantiate the JWT token valfdation middleware
-const isAuthenticated = jwt({
-    secret: process.env.TOKEN_SECRET,
-    algorithms: ["HS256"],
-    requestProperty: 'payload',
-    getToken: getTokenFromHeaders,
-});
+const jwt = require("jsonwebtoken");
 
+const isAuthenticated = async (req, res, next) => {
+  const token = req.headers.authorization;
+  //NOTE: if your token authentication is failing in Postman, uncomment the line below, and comment out the line above
+  // const token = req.headers.authorization?.split(" ")[1];
+  if (!token || token === "null") {
+    console.log("NO TOKEN");
+    return res.status(400).json({ message: "Token not found" });
+  }
+  try {
+    const tokenInfo = jwt.verify(token, process.env.TOKEN_SECRET);
+    console.log(tokenInfo);
+    //If you have req.payload, change line 12 to:
+    // req.payload = tokenInfo;
+    req.user = tokenInfo;
+    next();
+  } catch (error) {
+    return res.json(error);
+  }
+};
 
-function getTokenFromHeaders(req){
-    if(req.headers.authorization && req.headers.authorization.split(" ")[0] === "Bearer"){
-        const token = req.headers.authorization.split(" ")[1];
-        return token
-    }
-
-    return null
-}
-
-module.exports ={
-    isAuthenticated
-}
+// Export the middleware so that we can use it to create a protected routes
+module.exports = {
+  isAuthenticated,
+};
